e0aa9928b1535cdb69958469fc4d4e5b0ee59e0d
Nguyen Thuong Thong <nguyenthuongthongcm@gmail.com>
2022-03-30 20:42:25 +0700

    - Init project

---
+++ BoredApp/BoredApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BoredApp
+//
+//  Created by hungdat1234 on 3/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
